e87b1972888ada0b4c85d70a49ce1b73bac5ad26
Kristo <thebest2772005@gmail.com>
2026-05-30 14:59:36 +0700

    Demo

---
+++ build/CMakeFiles/main.dir/hello.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/hello.c.o: \
+ /home/kristo/practice/practice_cmake/hello.c /usr/include/stdc-predef.h \
+ /home/kristo/practice/practice_cmake/my_lib/hello.h
---
+++ my_lib/hellofunc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void print_hello(void){
+	printf("Hello cmake\n");
+	return;
+}
